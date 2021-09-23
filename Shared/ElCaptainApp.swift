944f4cd34a25bd49b83c28ad124e580af5116ecf
SOUABNI <issam.souabni@gmail.com>
2021-09-23 20:15:24 +0200

    New Project

---
+++ Shared/ElCaptainApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElCaptainApp.swift
+//  Shared
+//
+//  Created by Souabni Issam on 23/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct ElCaptainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
